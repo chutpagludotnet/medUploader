814252428b4dfeaa92c1071f59813795ed145097
chutpagludotnet <cluster@chutpaglu.net>
2025-06-04 01:36:47 +0300

    Create vars.py

---
+++ vars.py
@@ -0,0 +1,9 @@
+# vars.py
+import os
+from os import environ
+
+API_ID = int(environ.get("API_ID", "21311268"))
+API_HASH = environ.get("API_HASH", "af66e56cee1a95ddf34b2b5d1e422b14")
+BOT_TOKEN = environ.get("BOT_TOKEN", "8183551956:AAEUpy06v2xpaMPjLFM2JNRi9vrvYtaa-EU")
+OWNER = int(environ.get("OWNER", "7527795504"))
+CREDIT = "★彡[ᴍᴇᴅᴜꜱᴀxᴅ]彡★"
